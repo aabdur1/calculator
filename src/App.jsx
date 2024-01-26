673a77e512992adfef7aa4184c7b59c7d4c70e37
Amir Abdur-Rahim <amirabdurrahim@gmail.com>
2024-01-26 15:20:44 -0600

    Created number btn and operation components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import Number from './components/Number'
+import Operation from './components/Operation'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [screen, setScreen] = useState("")
+  const [operation, setOperation] = useState(null)
+  const store = []
+
+  function updateScreen(event){
+    store += event.target.value
+    setScreen(prevScreen => {
+      prevScreen += event.target.value
+    })
+  }
+
+  function clearScreen() {
+    setScreen("")
+  }
+
+  function operation(event) {
+    oldStore = store
+    store = []
+    setOperation(event.target.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='calculator'>
+      <div className="screen">{screen}</div>
+      <div className="firstRow">
+        <Number className="numBtn" value="7" handleClick={updateScreen}>7</Number>
+        <Number className="numBtn" value="8" handleClick={updateScreen}>8</Number>
+        <Number className="numBtn" value="9" handleClick={updateScreen}>9</Number>
+        <Operation className="clear" handleClick={clearScreen}>CLEAR</Operation>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="secondRow">
+        <Number className="numBtn" value="4" handleClick={updateScreen}>4</Number>
+        <Number className="numBtn" value="5" handleClick={updateScreen}>5</Number>
+        <Number className="numBtn" value="6" handleClick={updateScreen}>6</Number>
+        <Operation className="operation" handleClick={operation}>-</Operation>
+        <Operation className="operation" handleClick={operation}>+</Operation>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="bottomRows">
+        <div className="thirdRow">
+          <Number className="numBtn" value="1" handleClick={updateScreen}>1</Number>
+          <Number className="numBtn" value="2" handleClick={updateScreen}>2</Number>
+          <Number className="numBtn" value="3" handleClick={updateScreen}>3</Number>
+          <Operation className="operation" handleClick={operation}>X</Operation>
+        </div>
+        <div className="fourthRow">
+          <Number className="zero">0</Number>
+          <Number className="numBtn">.</Number>
+          <Operation className="operation" handleClick={operation}>/</Operation>
+        </div>
+        <div className="equals">
+          <Equals className="equals"></Equals>
+        </div>
+      </div>
+    </ div>
   )
 }
 
---
+++ src/components/Operation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Operation() {
+  return (
+    <button onClick={handleClick}></button>
+  )
+}
